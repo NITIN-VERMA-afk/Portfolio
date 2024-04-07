53cc150372a43b5989328537c2ea65ba7c0a9c45
Nitin Verma <nitinvermanv61506@gmail.com>
2024-04-07 22:43:16 +0530

    startAddingform

--- src/app/Incomponets/Navbar.tsx
+++ src/app/Incomponets/Navbar.tsx
@@ -1,42 +1,7 @@
-"use client"
-import React, { useState, useEffect } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar: React.FC = () => {
-  const [activeSection, setActiveSection] = useState<string>("");
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = document.querySelectorAll("section");
-      let minDistance = Infinity;
-      let activeSec = "";
-
-      sections.forEach((section) => {
-        const distance = Math.abs(section.getBoundingClientRect().top);
-        if (distance < minDistance) {
-          minDistance = distance;
-          activeSec = section.id;
-        }
-      });
-
-      setActiveSection(activeSec);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleLinkClick = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <div className="w-full h-12 bg-blue-600 rounded-md sticky top-0">
@@ -49,24 +14,24 @@ const Navbar: React.FC = () => {
 
           <ul className="hidden md:flex gap-x-6 text-white cursor-pointer">
             <li>
-              <a className={activeSection === "home" ? "active" : ""} onClick={() => handleLinkClick("home")}>Home</a>
+              <a href="#home">Home</a>
             </li>
             <li>
-              <a className={activeSection === "about" ? "active" : ""} onClick={() => handleLinkClick("about")}>About</a>
+              <a href="#about">About</a>
             </li>
             <li>
-              <a className={activeSection === "portfolio" ? "active" : ""} onClick={() => handleLinkClick("portfolio")}>Portfolio</a>
+              <a href="#portfolio">Portfolio</a>
             </li>
             <li>
-              <a className={activeSection === "services" ? "active" : ""} onClick={() => handleLinkClick("services")}>Services</a>
+              <a href="#services">Services</a>
             </li>
             <li>
-              <a className={activeSection === "experience" ? "active" : ""} onClick={() => handleLinkClick("experience")}>Experience</a>
+              <a href="#experience">Experience</a>
             </li>
           </ul>
 
           <Button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-            <a href="#contact" className={activeSection === "contact" ? "active" : ""} onClick={() => handleLinkClick("contact")}>Contact</a>
+            <a href="#contact">Contact</a>
           </Button>
         </div>
       </div>
@@ -75,6 +40,7 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
 
 
 
--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -1,78 +1,95 @@
-import React from 'react'
+import React from "react";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { IoLocation } from "react-icons/io5";
-import { Input } from "@/components/ui/input"
-import { Button } from '@/components/ui/button';
-import { Textarea } from "@/components/ui/textarea"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-
-
-
-interface ContactDetail {
-    id: number,
-    icon: JSX.Element,
-    details: string;
+interface Detail {
+  id: number;
+  icon: JSX.Element;
+  details: string;
 }
 
-const contactDetails=[
-    {
-        id:1,
-        icon:<FaPhoneSquareAlt />,
-        details:"9015308881"
-    },
-    {
-        id:2,
-        icon:<MdEmail />,
-        details:"nitinvermanv61506@gmail.com"
-    },
-    {
-        id:3,
-        icon:<IoLocation />,
-        details:"Bilaspur,h.p"
-    },
-    
-]
+const contactDetails: Detail[] = [
+  {
+    id: 1,
+    icon: <FaPhoneSquareAlt aria-label="phone" />,
+    details: "9015308881",
+  },
+  {
+    id: 2,
+    icon: <MdEmail aria-label="email" />,
+    details: "nitinvermanv61506@gmail.com",
+  },
+  {
+    id: 3,
+    icon: <IoLocation aria-label="location" />,
+    details: "Bilaspur, H.P.",
+  },
+];
 
-const Contact = () => {
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+const Contact: React.FC = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:nitinvermanv61506@gmail.com?subject=${formData.subject}&body=
+        Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
   return (
-    <div id='contact' className='pt-52'>
-        <div className='sm:rounded-sm  md:rounded-t-full bg-blue-600'>
-            <h1 className='text-white text-bold text-center text-2xl'>Contact</h1>
-            <div className='flex justify-around container mx-auto mt-5 pb-5 flex-wrap'>
-                <div className='flex flex-col gap-5'>
-                    <h1>Drop me a message</h1>
-                    <p className=''>contact me on follow details</p>
-                    {
-                        contactDetails.map(item=>(
-                            <div  key={item.id}>
-                                <ul className='flex gap-5'>
-                                    <li className='w-12 h-12 border border-gray-300 bg-gradient-to-br from-blue-600 to-blue-500 rounded flex justify-center items-center'>{item.icon}</li>
-                                    <li>{item.details}</li>
-                                </ul>
-
-                            </div>
-                        )
-                            
-                            )
-                    }
-                   
-
-                </div>
-                <div className=' flex flex-col gap-5 bg-blue-900 p-8 rounded-md '>
-                    <Input className='bg-black text-white' type='email' placeholder='Enter your email'/>
-                    <Input className='bg-black text-white' type='password' placeholder='Enter your password'/>
-                    <Textarea className='bg-black text-white' placeholder='message'/>
-                    <Button>Send</Button>
-
-
-                </div>
-            </div>
-
+    <div id="contact" className="pt-52">
+      <div className="sm:rounded-sm  md:rounded-t-full bg-blue-600">
+        <h1 className="text-white text-bold text-center text-2xl">Contact</h1>
+        <div className="flex justify-around container mx-auto mt-5 pb-5 flex-wrap">
+          <div className="flex flex-col gap-5">
+            <h1>Drop me a message</h1>
+            <p className="">contact me on follow details</p>
+            {contactDetails.map((item) => (
+              <div key={item.id}>
+                <ul className="flex gap-5">
+                  <li className="w-12 h-12 border border-gray-300 bg-gradient-to-br from-blue-600 to-blue-500 rounded flex justify-center items-center">
+                    {item.icon}
+                  </li>
+                  <li>{item.details}</li>
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className=" flex flex-col gap-5 bg-blue-900 p-8 rounded-md ">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                {...register("email")}
+                className="bg-black text-white"
+                type="email"
+                placeholder="Enter your email"
+              />
+              <Input
+                {...register("subject")}
+                className="bg-black text-white"
+                type="text"
+                placeholder="Enter your subject"
+              />
+              <Textarea
+                {...register("message")}
+                className="bg-black text-white"
+                placeholder="message"
+              />
+              <Button>Send</Button>
+            </form>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+
